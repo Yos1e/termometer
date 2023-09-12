99ff2acabd0817f054a9e4a5fec3c15585578a71
Yos1e <143427994+Yos1e@users.noreply.github.com>
2023-09-12 15:55:02 +0200

    Add README.md

---
+++ sketch_aug25c.ino
@@ -0,0 +1,204 @@
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+ 
+
+// Replace with your network credentials
+const char* ssid = "ABB_Gym_IOT";
+const char* password = "Welcome2abb";
+
+ 
+
+#define DHTPIN 2  // Digital pin connected to the DHT sensor
+
+ 
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+ 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+ 
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+ 
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+
+ 
+
+// Updates DHT readings every 2 seconds
+const long interval = 2000;  
+
+ 
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    body {
+      background-color: #F3F1CF;
+    }
+    h2 { font-size: 4.0rem; color: #69685D; }
+    p { font-size: 4.0rem; }
+    .units { font-size: 2.2rem; }
+    .dht-labels{
+      font-size: 2.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+</style>
+</head>
+<body>
+<h2>"YALLA TERMON"</h2>
+<p>
+<i class="fas fa-thermometer-half" style="color:#CD4240;"></i> 
+<span class="dht-labels">Temperature</span> 
+<span id="temperature">%TEMPERATURE%</span>
+<sup class="units">&deg;C</sup>
+</p>
+<p>
+<i class="fas fa-tint" style="color:#479DA4;"></i> 
+<span class="dht-labels">Humidity</span>
+<span id="humidity">%HUMIDITY%</span>
+<span id="today">Time</span>
+<sup class="units">%</sup>
+</p>
+<img src="https://ehtkollen.se/img/works/abb.png" width="300px" style="position: absolute; bottom: 75px; right: 100px;" />
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 2500 ) ;
+
+ 
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 2500 ) ;
+</script>
+</html>)rawliteral";
+
+ 
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return String(t);
+  }
+  else if(var == "HUMIDITY"){
+    return String(h);
+  }
+  return String();
+}
+
+ 
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(19200);
+  dht.begin();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
+  }
+
+ 
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+ 
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(t).c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(h).c_str());
+  });
+
+ 
+
+  // Start server
+  server.begin();
+}
+
+void loop(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      t = newT;
+      Serial.println(t);
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+      Serial.println(h);
+    }
+  }
+}
